7d8f1665540c88facf0c937b900f5a9f933c4fd0
Syed Tasavour <syedtasavour86@gmail.com>
2025-07-25 21:50:37 +0000

    Improves button and API handling with toast notifications
    Enhances button components to provide user feedback via toast notifications for API calls and custom actions.
    
    - Introduces toast notifications for success, error, and pending states of API requests within the `MsButton` and `MsConfirmButton` components.
    - Adds `toastConfig` prop to `MsButton` for displaying toasts on click and after custom actions.
    - Implements `useApiMutation` hook to handle API requests and toast notifications in `MsConfirmButton`, improving error handling.
    - Updates the API base URL in `.env`.
    - Adds `react-toastify` for global toast management.

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import 'semantic-ui-css/semantic.min.css';
 import './styles/globals.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <>
+      <App />
+      <ToastContainer />
+    </>
+  </BrowserRouter>
 );
