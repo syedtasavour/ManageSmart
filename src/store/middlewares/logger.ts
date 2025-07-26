5c35e22a58cfa86470e181c7863e645d13e56844
syed tasavour <syed.beneathatree@gmail.com>
2025-07-26 15:53:41 +0000

    Updates environment configuration and logger middleware
    - Changes the development mode flag in `.env` from `DEV=True` to `VITE_API_DEV=true`.
    - Adds `.env` to `.gitignore` to prevent it from being tracked.
    - Modifies the logger middleware to check the new `VITE_API_DEV` flag for logging actions in development mode.

--- src/store/middlewares/logger.ts
+++ src/store/middlewares/logger.ts
@@ -1,7 +1,7 @@
 import type { Middleware } from 'redux';
 
 export const loggerMiddleware: Middleware = _store => next => action => {
-  if (import.meta.env.DEV) {
+  if (import.meta.env.VITE_API_DEV === true) {
     console.group((action as any).type);
     console.info('dispatching', action);
     const result = next(action);
@@ -9,4 +9,4 @@ export const loggerMiddleware: Middleware = _store => next => action => {
     return result;
   }
   return next(action);
-}; 
+};
